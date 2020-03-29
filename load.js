e3612bea928f4356d2184b6967de5ad6530a4112
Patrick Lee Scott <pat@patscott.io>
2020-03-29 15:54:57 -0400

    feat: load test

---
+++ load.js
@@ -0,0 +1,23 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+
+const { url, users } = __ENV;
+
+export let options = {
+  stages: [
+    { duration: "5m", target: parseInt(users, 10) }, // simulate ramp-up of traffic from 1 to ${users} users over 5 minutes.
+    { duration: "10m", target: parseInt(users, 10) }, // stay at ${users} users for 10 minutes
+    { duration: "5m", target: 0 }, // ramp-down to 0 users
+  ],
+  thresholds: {
+    "http_req_duration": [{ threshold: "p(95)<500", abortOnFail: true }], // 95% of requests must complete below .5s
+  }
+};
+
+export default function() {
+  let res = http.get(url);
+  check(res, {
+    'status was 200': r => r.status == 200,
+  });
+  sleep(1)
+}
