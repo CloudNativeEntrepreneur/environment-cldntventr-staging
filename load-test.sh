be79b1a7814473642fd7b38dd92327aeef11fd12
Patrick Lee Scott <pat@patscott.io>
2020-03-29 20:55:47 -0400

    fix(k6): debug vault-config - use sh 2

--- load-test.sh
+++ load-test.sh
@@ -1,13 +1,14 @@
-eval $(jx get vault-config)
 
 curl -L https://github.com/starkandwayne/safe/releases/download/v1.3.4/safe-linux-amd64 --output ./safe-linux-amd64
 chmod +x ./safe-linux-amd64
 mv ./safe-linux-amd64 /usr/bin/safe
-echo "webhook url - $(safe get secret/staging/k6:slackUrl)"
 
 wget https://bintray.com/loadimpact/rpm/rpm -O bintray-loadimpact-rpm.repo
 mv bintray-loadimpact-rpm.repo /etc/yum.repos.d/
 yum -y install k6
+
+set -- $(jx get vault-config)
+echo "webhook url - $(safe get secret/staging/k6:slackUrl)"
 
 url=https://demo-app-jx-staging.cloudnativeentrepreneur.dev users=1 k6 --quiet --summary-export ./load-test-results run ./load-test.js
 slack_message=$(echo "Load Test Results: \n\tPasses: $(cat load-test-results | jq -r '.metrics | .checks.passes') \n\tFails: $(cat load-test-results | jq -r '.metrics | .checks.fails') \n\nTo see detailed results, check the build logs")
