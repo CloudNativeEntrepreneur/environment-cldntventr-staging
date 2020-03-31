7492247668a6d901aed0814edbadfe0e0190d3b7
Patrick Lee Scott <pat@patscott.io>
2020-03-31 14:53:24 -0400

    fix(k6): slack reporting 11

--- load-test.sh
+++ load-test.sh
@@ -10,19 +10,21 @@ wget https://bintray.com/loadimpact/rpm/rpm -O bintray-loadimpact-rpm.repo
 mv bintray-loadimpact-rpm.repo /etc/yum.repos.d/
 yum -y install k6
 
-echo "Configuring vault"
-jx get vault-config
+echo "Running Load Test..."
 
-eval `jx get vault-config`
+url=https://demo-app-jx-staging.cloudnativeentrepreneur.dev \
+users=1 \
+k6 --quiet --summary-export ./load-test-results run ./load-test.js
 
-echo "webhook url - $(safe get secret/staging/k6:slackUrl)"
-
-# slack_message="$(printf 'Load Test Results:\n---\n\n Passes: %s\n Fails: %s\n \nTo see detailed results, check the build logs with `jx get build logs "$JOB_NAME #$BUILD_NUMBER"`' \
-#   $(cat load-test-results | jq -r '.metrics | .checks.passes') \
-#   $(cat load-test-results | jq -r '.metrics | .checks.fails') \
-# )"
+slack_message="$(printf 'Load Test Results:\n---\n\n Passes: %s\n Fails: %s\n \nTo see detailed results, check the build logs with `jx get build logs "$JOB_NAME #$BUILD_NUMBER"`' \
+  $(cat load-test-results | jq -r '.metrics | .checks.passes') \
+  $(cat load-test-results | jq -r '.metrics | .checks.fails') \
+)"
 slack_message="hello"
 echo $slack_message
+
+echo "Configuring vault..."
+eval `jx get vault-config`
 
 curl --silent --data-urlencode \
   "$(printf 'payload={"text": "%s", "username": "%s", "as_user": "true", "link_names": "true", "icon_emoji": "%s" }' \
