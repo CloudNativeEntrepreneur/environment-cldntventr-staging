ad039806990af9644e5e2d9401e8ec06227ed8ea
Patrick Lee Scott <pat@patscott.io>
2020-03-31 14:21:00 -0400

    fix(k6): slack reporting 2

--- load-test.sh
+++ load-test.sh
@@ -1,20 +1,18 @@
 #!/bin/sh
 
+curl -L https://github.com/starkandwayne/safe/releases/download/v1.3.4/safe-linux-amd64 --output ./safe-linux-amd64
+chmod +x ./safe-linux-amd64
+mv ./safe-linux-amd64 /usr/bin/safe
 
-
-# curl -L https://github.com/starkandwayne/safe/releases/download/v1.3.4/safe-linux-amd64 --output ./safe-linux-amd64
-# chmod +x ./safe-linux-amd64
-# mv ./safe-linux-amd64 /usr/bin/safe
-
-# wget https://bintray.com/loadimpact/rpm/rpm -O bintray-loadimpact-rpm.repo
-# mv bintray-loadimpact-rpm.repo /etc/yum.repos.d/
-# yum -y install k6
+wget https://bintray.com/loadimpact/rpm/rpm -O bintray-loadimpact-rpm.repo
+mv bintray-loadimpact-rpm.repo /etc/yum.repos.d/
+yum -y install k6
 
 eval $(jx get vault-config)
 jx get vault config
 echo "webhook url - $(safe get secret/staging/k6:slackUrl)"
 
-slack_message="$(printf 'Load Test Results:\n---\n\n Passes: %s Fails: %s - To see detailed results, check the build logs with `jx get build logs`' \
+slack_message="$(printf 'Load Test Results:\n---\n\n Passes: %s\n Fails: %s\n \nTo see detailed results, check the build logs with `jx get build logs`' \
   $(cat load-test-results | jq -r '.metrics | .checks.passes') \
   $(cat load-test-results | jq -r '.metrics | .checks.fails') \
 )"
