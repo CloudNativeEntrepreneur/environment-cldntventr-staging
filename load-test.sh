75fc0857772307408f8e42c57867e006cde7b908
Patrick Lee Scott <pat@patscott.io>
2020-03-31 15:03:20 -0400

    fix(k6): slack reporting 14

--- load-test.sh
+++ load-test.sh
@@ -2,13 +2,13 @@
 
 echo "Downloading load testing tools..."
 
-curl -L https://github.com/starkandwayne/safe/releases/download/v1.3.4/safe-linux-amd64 --output ./safe-linux-amd64
-chmod +x ./safe-linux-amd64
-mv ./safe-linux-amd64 /usr/bin/safe
+# curl -L https://github.com/starkandwayne/safe/releases/download/v1.3.4/safe-linux-amd64 --output ./safe-linux-amd64
+# chmod +x ./safe-linux-amd64
+# mv ./safe-linux-amd64 /usr/bin/safe
 
-wget https://bintray.com/loadimpact/rpm/rpm -O bintray-loadimpact-rpm.repo
-mv bintray-loadimpact-rpm.repo /etc/yum.repos.d/
-yum -y install k6
+# wget https://bintray.com/loadimpact/rpm/rpm -O bintray-loadimpact-rpm.repo
+# mv bintray-loadimpact-rpm.repo /etc/yum.repos.d/
+# yum -y install k6
 
 echo "Configuring vault"
 jx get vault-config
@@ -21,9 +21,11 @@ url=https://demo-app-jx-staging.cloudnativeentrepreneur.dev \
 users=1 \
 k6 --quiet --summary-export ./load-test-results run ./load-test.js
 
-slack_message="$(printf 'Load Test Results:\n---\n\n Passes: %s\n Fails: %s\n \nTo see detailed results, check the build logs with `jx get build logs \"$JOB_NAME #$BUILD_NUMBER\"`' \
+slack_message="$(printf 'Load Test Results:\n---\n\n Passes: %s\n Fails: %s\n \nTo see detailed results, check the build logs with `jx get build logs`' \
   $(cat load-test-results | jq -r '.metrics | .checks.passes') \
   $(cat load-test-results | jq -r '.metrics | .checks.fails') \
+  "$JOB_NAME" \
+  "$BUILD_NUMBER" \
 )"
 # slack_message="hello"
 echo $slack_message
